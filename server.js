32f4fb035983f90f6260dbd690b99e8f5f699304
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-10 23:58:21 +0300

    feat: add auth register

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const connectDB = require("./models/connectDB");
 const router = require("./routes/router");
 
 connectDB();
+
+app.use(express.json);
+
 app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
