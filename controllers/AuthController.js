0aec4a308d877882c21d6733aac086d111ce2007
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-12 19:47:28 +0300

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,20 +1,32 @@
 const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 exports.authRegister = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  // TODO1: Validate the fields
-  // TODO2: check already registered
-  // TODO3: crpyt password
-  // TODO4: save the user to DB
+  const validationErr = validationResult(req);
+
+  if (validationErr?.errors?.length > 0) {
+    return res.status(400).json({ errors: validationErr.array() });
+  }
+
+  const userData = await User.findOne({ email });
+  if (userData) {
+    return res
+      .status(400)
+      .json({ errors: [{ message: "User already exists!!" }] });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(password, salt);
 
   const user = new User({
     firstName,
     lastName,
     email,
-    password, //crypted password
+    password: newPassword,
   });
-
   await user.save();
 
   //TODO: Error handling for saving
