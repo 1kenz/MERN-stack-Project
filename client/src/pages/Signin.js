4b134a6ada1a3c471d56e1e45e639ecf48cf5659
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-13 23:29:04 +0300

    feat: update client pages

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,7 +1,9 @@
+import { useContext } from "react";
 import { Form, Input, Button, Checkbox } from "antd";
-import { Layout } from "antd";
+import { postData } from "../helper/PostData";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
-const { Content } = Layout;
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
@@ -11,8 +13,15 @@ const tailLayout = {
 };
 
 const Signin = () => {
+  let history = useHistory();
+  const { setLoggedIn } = useContext(AuthContext);
+
   const onFinish = (values) => {
-    console.log("Success:", values);
+    postData("/api/auth/login", values).then((data) => {
+      localStorage.setItem("token", data.token);
+      setLoggedIn(true);
+      history.push("/");
+    });
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -20,41 +29,41 @@ const Signin = () => {
   };
 
   return (
-      <div>
-        <Form
-          {...layout}
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
+    <div>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
         >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
-          >
-            <Input />
-          </Form.Item>
+          <Input />
+        </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
